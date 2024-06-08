1095eaaa3a0da9a7f319ee54a8afb4d36263c1ed
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2024-06-08 13:01:37 +0200

    VertexPathingUtilities class [Graphs];

---
+++ src/Graphs/Utilities/VertexPathingUtilities.cs
@@ -0,0 +1,31 @@
+﻿using Graphs.DataStructures;
+using Graphs.Models;
+using System.Runtime.InteropServices;
+
+namespace Graphs.Utilities;
+
+internal static class VertexPathingUtilities
+{
+    public static void ResetPaths(Vertex vertex, List<Vertex> vertices)
+    {
+        vertex.Paths.Clear();
+        ReadOnlySpan<Vertex> vertexSpan = CollectionsMarshal.AsSpan(vertices);
+
+        for (int i = 0; i <= vertices.Count - 1; i++)
+        {
+            Pathing pathing = new()
+            {
+                TargetVertexId = vertexSpan[i].Id,
+                TotalWeight = float.PositiveInfinity,
+                VertexIds = new()
+            };
+
+            if (pathing.TargetVertexId == vertex.Id)
+            {
+                pathing.TotalWeight = 0;
+            }
+
+            vertex.Paths.Add(pathing);
+        }
+    }
+}
