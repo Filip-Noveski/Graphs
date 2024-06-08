d7775b0bcf4aac76d5f7eec2b4c5e7a16070b934
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2024-06-08 11:48:38 +0200

    Id Not Found exception class [Graphs]

---
+++ src/Graphs/Exceptions/IdNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Graphs.Exceptions;
+
+public class IdNotFoundException : Exception
+{
+    public IdNotFoundException(string entityType, char id)
+        : base($"The entity of type '{entityType}' with the id '{id}' was not found")
+    {
+        
+    }
+}
