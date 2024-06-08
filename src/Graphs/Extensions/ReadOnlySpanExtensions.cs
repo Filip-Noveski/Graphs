0bb602d9cd95bc382f6c7543cf352142b57a9c9c
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2024-06-08 13:01:58 +0200

    ReadOnlySpan extensions [Graphs];

---
+++ src/Graphs/Extensions/ReadOnlySpanExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Graphs.Extensions;
+
+public static class ReadOnlySpanExtensions
+{
+    public static T Dequeue<T>(this ReadOnlySpan<T> span, out ReadOnlySpan<T> newSpan)
+    {
+        newSpan = span[1..];
+        return span[0];
+    }
+}
