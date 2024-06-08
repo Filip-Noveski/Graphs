b018a92c024f432c10f609d2a27eac3cfe46d816
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2024-06-08 13:02:04 +0200

    List extensions [Graphs];

---
+++ src/Graphs/Extensions/ListExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Graphs.Extensions;
+
+public static class ListExtensions
+{
+    public static bool Contains<T>(this List<T> list, Func<T, bool> predicate)
+    {
+        return list.Where(predicate).Any();
+    }
+
+    public static void Remove<T>(this List<T> list, Func<T, bool> predicate)
+    {
+        T val = list.First(predicate);
+        list.Remove(val);
+    }
+}
