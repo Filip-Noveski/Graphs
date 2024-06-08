f6e7738b5eff7079bb2aa057dba937c0293d8ed5
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2024-06-08 12:22:50 +0200

    Graph class, not implemented [Graphs]

---
+++ src/Graphs/DataStructures/Graph.cs
@@ -0,0 +1,64 @@
+﻿using Graphs.Interfaces;
+
+namespace Graphs.DataStructures;
+
+/// <summary>
+/// Graph data structure.
+/// </summary>
+public class Graph : IGraph
+{
+    public void CreateVertex(char id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteVertex(char id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CreateEdge(char id, char sourceVertex, char targetVertex, int weight)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteEdge(char id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateEdgeWeight(char id, int newWeight)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void BellmanFord()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void BellmanFord(char sourceVertexId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DependencyListSP()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DependencyListSP(char sourceVertexId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void QueuedSP()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void QueuedSP(char sourceVertexId)
+    {
+        throw new NotImplementedException();
+    }
+}
