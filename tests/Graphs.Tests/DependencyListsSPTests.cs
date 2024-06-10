6a3a80ff545b62c54296c1ca857e0701ab133944
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2024-06-10 23:08:44 +0200

    DependecyListSPTests [Graphs.Tests];

---
+++ tests/Graphs.Tests/DependencyListsSPTests.cs
@@ -0,0 +1,31 @@
+﻿using Graphs.DataStructures;
+using Graphs.Exceptions;
+using Graphs.Tests.Helpers;
+
+namespace Graphs.Tests;
+
+public class DependencyListsSPTests : ShortestPathsTests
+{
+    [Fact]
+    public void ShouldFindShortestPathsFromA()
+    {
+        // Arrange
+        Graph graph = GraphWithSolution;
+
+        // Act
+        graph.DependencyListSP('A');
+
+        // Assert
+        AssertGraphWithSolutionForSourceA(graph);
+    }
+
+    [Fact]
+    public void ShouldThrowNegativeWeightCycleFromA()
+    {
+        // Arrange
+        Graph graph = GraphWithNWCycle;
+
+        // Act / Assert
+        Assert.Throws<NegativeWeightCycleException>(() => graph.DependencyListSP('A'));
+    }
+}
