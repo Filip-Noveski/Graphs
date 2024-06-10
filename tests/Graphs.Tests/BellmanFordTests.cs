7ea245452ef0dc8072b80152f5e41f57cc043524
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2024-06-10 23:08:29 +0200

    BellmanFordTests [Graphs.Tests];

---
+++ tests/Graphs.Tests/BellmanFordTests.cs
@@ -0,0 +1,31 @@
+﻿using Graphs.DataStructures;
+using Graphs.Exceptions;
+using Graphs.Tests.Helpers;
+
+namespace Graphs.Tests;
+
+public class BellmanFordTests : ShortestPathsTests
+{
+    [Fact]
+    public void ShouldFindShortestPathsFromA()
+    {
+        // Arrange
+        Graph graph = GraphWithSolution;
+
+        // Act
+        graph.BellmanFord('A');
+
+        // Assert
+        AssertGraphWithSolutionForSourceA(graph);
+    }
+
+    [Fact]
+    public void ShouldThrowNegativeWeightCycleFromA()
+    {
+        // Arrange
+        Graph graph = GraphWithNWCycle;
+
+        // Act / Assert
+        Assert.Throws<NegativeWeightCycleException>(() => graph.BellmanFord('A'));
+    }
+}
