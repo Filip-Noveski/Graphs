f2d4961869d8dd0c454401df8228c2cd648205c2
Filip Noveski <filip.noveski@cse.uist.edu.mk>
2024-06-10 22:38:54 +0200

    NegativeWeightCycleException class [Graphs];

---
+++ src/Graphs/Exceptions/NegativeWeightCycleException.cs
@@ -0,0 +1,15 @@
+﻿namespace Graphs.Exceptions;
+
+/// <summary>
+/// Represents an error that there was a negative-weight cycle in the graph 
+/// where shortest paths were calculated.
+/// </summary>
+public class NegativeWeightCycleException : Exception
+{
+    /// <summary>
+    /// Creates an instance of the <see cref="NegativeWeightCycleException"/> class.
+    /// </summary>
+    public NegativeWeightCycleException() : base($"There was a negative-weight cycle")
+    {
+    }
+}
